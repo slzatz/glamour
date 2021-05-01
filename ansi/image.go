d1d213636604579813580e36bf673a7ea38b50d0
Steve Zatz <slzatz@gmail.com>
2021-05-01 18:01:12 -0400

    ansi/image.go kitty - starting to try to display images

--- ansi/image.go
+++ ansi/image.go
@@ -1,7 +1,12 @@
 package ansi
 
 import (
+	//	"bufio"
+	"fmt"
+	"github.com/slzatz/glamour/kitty"
 	"io"
+	"log"
+	"os"
 )
 
 // An ImageElement is used to render images elements.
@@ -33,6 +38,24 @@ func (e *ImageElement) Render(w io.Writer, ctx RenderContext) error {
 		if err != nil {
 			return err
 		}
+		/*
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Printf("\x1b[6n")
+			text, _ := reader.ReadLine()
+		*/
+
+		//fmt.Printf("\x1b[%d;%dH", 20, 100)
+		fmt.Println("Hello World")
+		iImg, _, err := kitty.loadImage("/home/slzatz/Pictures/wood_ducks_smaller.jpg")
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = kitty.KittyWriteImage(os.Stdout, iImg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("")
+		//fmt.Println(text)
 	}
 
 	return nil
